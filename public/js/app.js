8ca8202d61c7d535d593d608fc527f625cc46f14
seraphicgate <fabian_gzh@hotmail.com>
2020-05-31 21:41:10 +0800

    Remove unnecessary lines in app.js in js

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,7 @@
-console.log("client side javascript file is loaded")
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input[name="input2"]')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
-
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
